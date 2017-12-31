bb291ac1b11b137ab2934f6720aff064b911c238
Oliver Tanner <ollie@niume.co.uk>
2017-12-31 11:51:34 +0000

    Added php file to connect to database

---
+++ dblogin.php
@@ -0,0 +1,18 @@
+<?php
+function dblogin(){
+  $username="root";
+  $password="t16rMSpw";
+  $host='localhost';
+  $dbName='make_it_all';
+
+  $dsn = "mysql://$username:$password@$host/$dbName"; //Data Source Name
+
+  require_once('MDB2.php');
+  $db =& MDB2::connect($dsn); //try to make a connection
+
+  if (PEAR::isError($db)) { //if error connecting
+      die($db->getMessage());
+  }
+  return $db; //return database object
+}
+?>
