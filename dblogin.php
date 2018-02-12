ab6d3f37f214ac69c72165fc4edb629e72fc87be
Oliver Tanner <ollie@niume.co.uk>
2018-02-12 14:30:48 +0000

    Updated database login function

--- dblogin.php
+++ dblogin.php
@@ -5,15 +5,9 @@ function dblogin(){
   $host='localhost';
   $dbName='make_it_all';
 
-  $dsn = "mysql://$username:$password@$host/$dbName"; //Data Source Name
+  $db = mysqli_connect($host,$username,$password,$dbName)
+ or die('Error connecting to MySQL server.');
 
-  require_once('MDB2.php');
-  $db =& MDB2::connect($dsn); //try to make a connection
-
-  if (PEAR::isError($db)) { //if error connecting
-      die($db->getMessage());
-  }
-  $db->setFetchMode(MDB2_FETCHMODE_ASSOC);
   return $db; //return database object
 }
 ?>
