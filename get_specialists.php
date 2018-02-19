84dc1eed918cde5fc47cf86fe6977ee63ea17d50
Oliver Tanner <ollie@niume.co.uk>
2018-02-19 07:41:47 +0000

    Finished specialist lookup functionality

--- get_specialists.php
+++ get_specialists.php
@@ -2,36 +2,76 @@
   session_start();
   header("Content-Type: text/html");
   include 'dblogin.php';
-
+  $targetptid = $_POST["targetptid"];
   $db = dblogin();
 
-//  $sql = "SELECT 'sp.id' AS specid, 'st.name' AS specname, 'p.priority' AS priority
-  //FROM `specialists` AS s
-  //LEFT JOIN `staff` AS st ON 'sp.staff_id' = 'st.id'
-  //LEFT JOIN `problems` AS p ON 's.id' = 'p.specialist_id';";
+  $ptsql = "SELECT pt1.id, pt1.name, pt1.broad_type_id, pt2.name
+  FROM `problem_types` AS pt1 LEFT JOIN `problem_types` AS pt2
+  ON pt1.broad_type_id = pt2.id
+  WHERE pt1.id = '$targetptid';";
+  $broadid;
+  $ptresult = $db->query($ptsql);
 
-  $sql = "SELECT specialists.id AS 'specid', staff.name AS 'specname', problems.priority AS 'priority'
+  if ($ptresult->num_rows > 0) {
+    // output data of each row
+    while($row = $ptresult->fetch_assoc()) {
+      $broadid = $row["broad_type_id"];
+    }
+  }
+
+  $sptsql = "SELECT * FROM `specialist_problem_type`;";
+
+  $sql = "SELECT specialists.id AS specid, staff.name AS specname, problems.priority AS priority
     FROM `specialists`
     LEFT JOIN `staff` ON specialists.staff_id = staff.id
     LEFT JOIN `problems` ON specialists.id = problems.specialist_id;";
 
   $result = $db->query($sql);
-  $workload = array();
+
+  $specialists = array();
   if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
+
       $specid = $row["specid"];
       $specname = $row["specname"];
       $priority = $row["priority"];
-      if ($priority>0) {
-        if ($workload[$specname] != NULL) {
-          $workload[$specname] += $priority;
-        } else {
-          $workload[$specname] = $priority;
+      $found = false;
+      foreach ($specialists as $specialist) {
+        if ($specialist["specid"] == $specid) {
+          $found = true;
+          if ($priority>0) {
+            $specialist["priority"] += $priority;
+          }
         }
-      } else {
-        if ($workload[$specname] == NULL) {
-          $workload[$specname] = 0;
+
+    }
+    if (!$found){
+      $arr = array(
+        "specid" => $specid,
+        "specname" => $specname,
+        "priority" => ($priority == NULL) ? 0 : $priority,
+        "ptid" => ""
+      );
+      array_push($specialists, $arr);
+    }
+  }
+
+    $sptresult = $db->query($sptsql);
+    if ($sptresult->num_rows > 0) {
+      // output data of each row
+      while($row = $sptresult->fetch_assoc()) {
+        $currptid = $row["problem_type_id"];
+        $currspid = $row["specialist_id"];
+        if ($currptid == $targetptid || $currptid == $broadid){
+          foreach ($specialists as &$specialist) {
+            if ($specialist["specid"] == $currspid){
+              if ($specialist["ptid"] != $targetptid){
+                $specialist["ptid"] = $currptid;
+              }
+            }
+            unset($specialist);
+          }
         }
       }
     }
@@ -43,7 +83,7 @@
     $db->close();
     exit;
   }
-  echo json_encode($workload);
+  echo json_encode($specialists);
   $db->close();
   exit();
  ?>
